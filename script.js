201cea57f817387ed61404865d3bee49b00675d6
Natasha <nataly815@gmail.com>
2020-12-07 11:58:02 +0300

    updated code style due to codacy

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-'use strict';
+"use strict";
 
-const optionCollection = document.querySelectorAll('.option');
+const optionCollection = document.querySelectorAll(".option");
 
-const startBtn = document.getElementById('start-button');
-const gameWrap = document.querySelector('.game-wrap');
-const menu = document.querySelector('.start-wrap');
+const startBtn = document.getElementById("start-button");
+const gameWrap = document.querySelector(".game-wrap");
+const menu = document.querySelector(".start-wrap");
 
 let select = optionCollection[0];
 
 function optionSelect() {
   if(select) {
-    select.classList.toggle('active');
+    select.classList.toggle("active");
   }
-  this.classList.toggle('active');
+  this.classList.toggle("active");
   select = this;
   }
 
 for(let el of optionCollection){
-  el.addEventListener('click', optionSelect);
+  el.addEventListener("click", optionSelect);
 }
 
 function createCard(el, cards) {
-  const tableCards = document.createElement('div');
+  const tableCards = document.createElement("div");
   tableCards.className = `table_cards ${cards}`;
   for (let i=0; i < el; i++){
-    const card = document.createElement('div');
-    card.className = 'card';
+    const card = document.createElement("div");
+    card.className = "card";
     card.innerHTML = `
     <div class="back-card"></div>
     <div class="gameover-card"></div>
@@ -35,50 +35,46 @@ function createCard(el, cards) {
   gameWrap.appendChild(tableCards);
 }
 
-function startGame() {
-  console.log(`Начали игру. ${select.dataset.name} уровень`);
-  if(select.dataset.name === 'легкий'){
-    createCard(3, 'three-card');
-    init();
-    menu.style.display = "none";
-    } else if(select.dataset.name === 'средний'){
-    createCard(6, 'six-card');
-    init();
-    menu.style.display = "none";
-    } else {
-    createCard(10, 'ten-card');
-    init();
-    menu.style.display = "none";
-  }
-}
-
-startBtn.addEventListener('click', startGame);
-
 function returnToMenu(){
-  let tableField = document.querySelector('.table_cards');
+  let tableField = document.querySelector(".table_cards");
   tableField.remove(tableField.children);
   menu.style.display = "block";
 }
 
 function cardClick(){
-  let cards = document.querySelectorAll('.card');
-  this.classList.add('flip');
-  cards.forEach(item =>
-    item.addEventListener('click', returnToMenu));
+  let cards = document.querySelectorAll(".card");
+  this.classList.add("flip");
+  cards.forEach(item => item.addEventListener("click", returnToMenu));
 }
 
 function init(){
-  let cards = document.querySelectorAll('.card');
-  console.log(`Кол-во карт на столе - ${cards.length}`);
+  let cards = document.querySelectorAll(".card");
 
   function getRandomBug(max){
     let i = Math.floor(Math.random() * Math.floor(max));
-    cards[i].children[1].style.backgroundImage = 'url(./img/Card-bug.png)';
-    console.log(`рандомная цифра из количества карт - ${i}`);
+    cards[i].children[1].style.backgroundImage = "url(./img/Card-bug.png)";
   }
   getRandomBug(cards.length);
 
-    cards.forEach(item =>
-    item.addEventListener('click', cardClick));
+    cards.forEach(item => item.addEventListener("click", cardClick));
 }
+
+function startGame() {
+  if(select.dataset.name === "легкий"){
+    createCard(3, "three-card");
+    init();
+    menu.style.display = "none";
+    } else if(select.dataset.name === "средний"){
+    createCard(6, "six-card");
+    init();
+    menu.style.display = "none";
+    } else {
+    createCard(10, "ten-card");
+    init();
+    menu.style.display = "none";
+  }
+}
+
+startBtn.addEventListener("click", startGame);
+
 
